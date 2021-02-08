dbe601ac4329f17c7e0d73a2d2543dff5e04122e
GWilczyk <gwilczyk.github@gmail.com>
2021-02-08 14:28:40 +0100

    Implemente Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import Navbar from './components/Navbar';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import Particles from 'react-particles-js';
+
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<>
+			<Particles
+				className='particles-canvas'
+				params={{
+					particles: {
+						number: {
+							value: 120,
+							density: {
+								enable: true,
+							},
+						},
+						size: {
+							value: 0,
+						},
+					},
+					interactivity: {
+						events: {
+							onhover: {
+								enable: true,
+								mode: 'repulse',
+							},
+						},
+					},
+				}}
+			/>
 			<Navbar />
-			<h2>Portfolio Project…</h2>
+			<Header />
 		</>
 	);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
 	return (
-		<nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+		<nav className='navbar navbar-expand-lg'>
 			<div className='container'>
 				<a className='navbar-brand' href='#'>
 					<img src={logo} alt='Logo' className='logo' />
